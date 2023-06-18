d5d7a825f7bb8504af55565182a83c1a28dbb4e8
abdulsamad42232 <abdulsamadkhan2001@gmail.com>
2023-06-18 14:42:53 +0500

    add .ipynb.

---
+++ concepts of list and tuple.py
@@ -0,0 +1,25 @@
+#complte lecture on link tinymoaazzam.something
+
+
+# Cities=["karachi","Lahore","Islamabad"]
+# print(Cities[-3])
+# Cities.append("Quetta")
+# print(Cities[-1])
+
+
+# Cities=["karachi","Lahore","Islamabad"]
+# Cities.insert(0,"NewYork")
+# print(Cities)
+
+# Cities=["karachi","Lahore","Islamabad"]
+# Cities[2]="houston"
+# print(Cities)
+
+
+Cities=["karachi","Lahore","Islamabad"]
+a=Cities[0:1:1]
+print(a)
+
+# Cities=["karachi","Lahore","Islamabad"]
+
+
---
+++ loops.py
@@ -0,0 +1,115 @@
+# for i in range(10):
+#     print(i)
+# 
+# output
+# 
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+
+# for i in range(1,11):
+#     print("2 x",i,"=",2*i)
+
+#output
+     
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10 = 20    
+    
+# a=int(input("Enter any number: "))
+# b=int(input("Enter your range: "))
+# c=b+1
+# for i in range(1,c):
+#     print(a,"x",i,"=",a*i)
+
+#output
+    
+# Enter any number: 6
+# Enter your range: 15
+# 6 x 1 = 6
+# 6 x 2 = 12
+# 6 x 3 = 18
+# 6 x 4 = 24
+# 6 x 5 = 30
+# 6 x 6 = 36
+# 6 x 7 = 42
+# 6 x 8 = 48
+# 6 x 9 = 54
+# 6 x 10 = 60
+# 6 x 15 = 90
+# 6 x 11 = 66
+# 6 x 12 = 72
+# 6 x 13 = 78
+# 6 x 14 = 84
+# 6 x 15 = 90
+
+
+# all_cities=["karachi","islamabad","lahore"]
+# city="karachi"
+# for i in all_cities:
+#     if i==city:
+#         print(i,"is clean City")
+#     else:
+#         print(i,"is not clean City")
+
+# output
+ 
+# karachi is clean City
+# islamabad is not clean City
+# lahore is not clean City
+
+# a=12345
+# b=str(a)
+# a=b[::-1]
+# print(a)
+# 
+# output
+# 54321
+
+
+# a=str(input("Enter Armstrong num: "))
+# dig=len(a)
+# result=0
+# for i in a:
+#     result= result + int(i)**dig
+# print(result)
+# 
+# if result==int(a):
+#     print("correct")
+# else:
+#     print("incorrect")
+
+# #output
+# Enter Armstrong num: 371
+# 371
+# correct    
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
